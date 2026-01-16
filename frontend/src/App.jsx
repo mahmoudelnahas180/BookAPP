efc4b14fd88246238b7882feb98a699776f90ef0
mahmoudelnahas180 <mahmoudelnahas180@gmail.com>
2026-01-16 16:58:33 +0200

    hide header is user click login or sign up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Login from './pages/Login'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 function App() {
+  const location = useLocation()
+  const hideHeaderRoutes = ["/login", "/signup"]
+  const hideHeader = hideHeaderRoutes.includes(location.pathname)
 
   return (
     <>
-      <Header />
+      {!hideHeader && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
