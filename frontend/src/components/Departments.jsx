3ca8f819f6da1f12d9e01a589cfb26e034e84924
mahmoudelnahas180 <mahmoudelnahas180@gmail.com>
2026-01-22 21:10:40 +0200

    Add Departemnets

---
+++ frontend/src/components/Departments.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+
+/**
+ * @component Departments
+ * @description الصفحة الرئيسية للتطبيق.
+ * تعمل كواجهة هبوط (Landing Page) وتعرض المكونات الترويجية مثل Hero Section.
+ */
+export default function Departments() {
+  return (
+    <>
+        <h1>Departments</h1>        
+      
+    </>
+  )
+}
