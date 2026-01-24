4cd7a6cf03e603d1e343e07c3b5152edcbf31d2f
mahmoudelnahas180 <mahmoudelnahas180@gmail.com>
2026-01-24 22:14:35 +0200

    feat: Implement initial book management API and frontend components for book and category display.

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -4,39 +4,47 @@
  * يشمل عمليات CRUD: إضافة، عرض، تعديل، وحذف الكتب.
  */
 
-const express = require('express');
+const express = require("express");
 const bookrouter = express.Router();
 const asyncWrapper = require("../utils/asyncWrapper");
-const BookController = require('../controller/bookController');
+const BookController = require("../controller/bookController");
 
 /**
  * @route POST /books
  * @description إضافة كتاب جديد.
  */
-bookrouter.post('/books', asyncWrapper(BookController.addBook));
+bookrouter.post("/books", asyncWrapper(BookController.addBook));
 
 /**
  * @route GET /getbooks
  * @description جلب جميع الكتب.
  */
-bookrouter.get('/getbooks', asyncWrapper(BookController.getAllBooks));
+bookrouter.get("/getbooks", asyncWrapper(BookController.getAllBooks));
 
+/**
+ * @route GET /getlastfivebooks
+ * @description جلب آخر 5 كتب تم إضافتها.
+ */
+bookrouter.get(
+  "/getlastfivebooks",
+  asyncWrapper(BookController.getLastFiveBooks),
+);
 /**
  * @route GET /books/:id
  * @description جلب كتاب محدد بواسطة الـ ID.
  */
-bookrouter.get('/books/:id', asyncWrapper(BookController.getBookById));
+bookrouter.get("/books/:id", asyncWrapper(BookController.getBookById));
 
 /**
  * @route PUT /books/:id
  * @description تعديل بيانات كتاب موجود.
  */
-bookrouter.put('/books/:id', asyncWrapper(BookController.updateBook));
+bookrouter.put("/books/:id", asyncWrapper(BookController.updateBook));
 
 /**
  * @route DELETE /books/:id
  * @description حذف كتاب.
  */
-bookrouter.delete('/books/:id', asyncWrapper(BookController.deleteBook));
+bookrouter.delete("/books/:id", asyncWrapper(BookController.deleteBook));
 
 module.exports = bookrouter;
